3f283d3a30f2dd1de5427c5dc0c4c59274fe57d6
Mheeh <dreekus@gmail.com>
2017-01-24 07:07:05 +0100

    Implement StudentDecorator.avg_notes

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,12 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = subject_item_notes.for_subject_item(subject_item).map {|s| s.value}
+
+    if notes.empty?
+      "0.00"
+    else
+      '%.2f' % (notes.inject(:+).to_f / notes.length).round(2)
+    end
   end
 end
